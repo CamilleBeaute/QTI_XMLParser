03fc280feb036b35b5daa39f40caf90ef9edbcaf
Alyxis Johnson <alyxis@Alyxis-MacBook-Pro.local>
2020-05-14 19:37:10 -0400

    Loop through array to display question titles

--- parser.php
+++ parser.php
@@ -5,14 +5,55 @@
     <title>QTI XML Parser</title>
   </head>
   <body>
-    <pre>
+    <!--pre-->
       <?php
         $filename = 'example_qti.xml';
 
         $xml = simplexml_load_file($filename) or die("Error: Cannot create object");
-        print_r($xml);
+        //print_r($xml);
+
+        //assessment array
+        foreach($xml as $assessment) {
+          //print_r($assessment);
+
+          //section array
+          foreach($assessment as $section) {
+            //print_r($section);
+
+            //item array
+            foreach($section as $key=>$value) {
+              if($key == 'item') {
+                //print_r($value);
+
+                //presentation array
+                foreach($value as $key=>$value) {
+                  if($key == 'presentation') {
+
+                    //material array
+                    foreach ($value as $key=>$value) {
+                      if($key == 'material') {
+                        //print_r($value);
+
+                        //mattext
+                        foreach($value as $key=>$value) {
+                          echo($value);
+                          echo('<br>');
+                        }
+
+                      }
+
+                    }
+
+                  }
+                }
+
+              }
+            }
+
+          }
+        }
 
       ?>
-    </pre>
+    <!--/pre-->
   </body>
 </html>
