23b42a24ccae4323f5bbce75982f9ffe6fc3f967
Alyxis Johnson <alyxis@Alyxis-MacBook-Pro.local>
2020-05-14 13:11:39 -0400

    Read & output XML from file

--- parser.php
+++ parser.php
@@ -5,22 +5,14 @@
     <title>QTI XML Parser</title>
   </head>
   <body>
-
+    <pre>
       <?php
+        $filename = 'example_qti.xml';
 
-        $myXMLData =
-          "<?xml version='1.0' encoding='UTF-8'?>
-          <note>
-          <to>Tove</to>
-          <from>Jani</from>
-          <heading>Reminder</heading>
-          <body>Don't forget me this weekend!</body>
-          </note>";
+        $xml = simplexml_load_file($filename) or die("Error: Cannot create object");
+        print_r($xml);
 
-          $xml = simplexml_load_string($myXMLData) or die("Error: Cannot create object");
-          print_r($xml);
-          
       ?>
-
+    </pre>
   </body>
 </html>
